9bf456d435f4c70194e6fa2446dcd79a1713526c
Hayden McAfee <hayden@outlook.com>
2017-11-18 18:22:42 -0800

    Ehhh, screw React.

--- Tunr/Client/data/ApplicationStore.ts
+++
@@ -1,7 +0,0 @@
-import { observable } from "mobx";
-import { LoginStore } from "./LoginStore";
-
-export class ApplicationStore {
-    @observable
-    public loginStore: LoginStore = new LoginStore();
-}
