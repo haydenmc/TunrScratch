0b20c685aae35e80159e817ee5951ec0bc8a8e7c
Hayden McAfee <hayden@outlook.com>
2017-12-25 18:12:26 -0600

    Visualizer renders a properly scaled image now!

--- Tunr/Client/Scripts/Components/Visualizer.ts
+++ Tunr/Client/Scripts/Components/Visualizer.ts
@@ -1,8 +1,23 @@
 import { Component } from "../Component";
 
 export class Visualizer extends Component {
+    private readonly imageScaleFactor: number = 1.25;
+    private currentImage: HTMLImageElement;
+    private currentImageIsLoaded: boolean;
+
     constructor() {
         super("Visualizer");
+        // Test
+        this.setImage("/Assets/Images/TestArtistImage.jpg");
+    }
+
+    public setImage(url: string): void {
+        this.currentImageIsLoaded = false;
+        this.currentImage = new Image();
+        this.currentImage.src = url;
+        this.currentImage.onload = () => {
+            this.currentImageIsLoaded = true;
+        };
     }
 
     protected componentInserted() {
@@ -17,8 +32,27 @@ export class Visualizer extends Component {
         canvas.height = canvas.parentElement.clientHeight;
         // Draw
         let context = canvas.getContext("2d");
-        context.fillStyle = "#ff0000";
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        if (this.currentImageIsLoaded) {
+            // calculate draw dimensions
+            let canvasAspect = canvas.width / canvas.height;
+            let imageAspect = this.currentImage.naturalWidth / this.currentImage.naturalHeight;
+            let scaleFactor: number;
+            if (imageAspect >= canvasAspect) {
+                // scale to match canvas height
+                scaleFactor = (canvas.height / this.currentImage.naturalHeight);
+            } else {
+                // scale to match canvas width
+                scaleFactor = (canvas.width / this.currentImage.naturalWidth);
+            }
+            // Adjust scale factor for zoom
+            scaleFactor *= this.imageScaleFactor;
+            let destinationWidth = this.currentImage.naturalWidth * scaleFactor;
+            let destinationHeight = this.currentImage.naturalHeight * scaleFactor;
+            // Randomize padding
+            //let startX = -1 * Math.random() * (destinationWidth - canvas.width);
+            //let startY = -1 * Math.random() * (destinationHeight - canvas.height);
+            context.drawImage(this.currentImage, 0, 0, destinationWidth, destinationHeight); // startX, startY
+        }
         requestAnimationFrame(() => this.draw());
     }
 }
